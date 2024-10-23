70288a60c445a4343d1c52764b447330cf3868d3
cristian <cp1031716@gmail.com>
2024-10-22 22:43:16 -0400

    Calcular y limpiar calculadora

--- script.js
+++ script.js
@@ -32,3 +32,44 @@ function handleInput(value) {
         updateDisplay();
     }
 }
+
+function calculate() {
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operator) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+
+    currentInput = result;
+    operator = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function clear() {
+    currentInput = '';
+    operator = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.value = currentInput;
+}
